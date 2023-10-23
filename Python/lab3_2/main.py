f03c6af5243738b10e3e81abd86ca2fe750d5e6e
Ihor Mykytiuk <igor14vn@gmail.com>
2023-10-23 20:03:46 +0300

    Summary

--- Python/lab3_2/main.py
+++ Python/lab3_2/main.py
@@ -1,4 +1,2 @@
-coefficient = (100-0) / (212-32)
-fahrenheit = float(input('Введіть значення за шкалою Фаренгейта: '))
-celsius = (fahrenheit - 32) * coefficient
-print('Значення за шкалою Цельсія: ', celsius)
+
+print(18 and 'qwerty' or True)
--- Python/lab9_1/main.py
+++ Python/lab9_1/main.py
@@ -1,5 +1,8 @@
-def blackjack(cards):
-    cards_scores = {i: i for i in range(2, 11)}
-    cards_scores.update({'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': (1, 11)})
+x = True
+y = False
+z = False
 
-
+if x or y and z:
+    print('Yes')
+else:
+    print('No')
