ace11e5f58fcd746af045175fd51bd1790c17b66
Ihor Mykytiuk <igor14vn@gmail.com>
2023-10-23 20:59:29 +0300

    complete lab 9_1

--- Python/lab9_1/main.py
+++ Python/lab9_1/main.py
@@ -1,8 +1,27 @@
-x = True
-y = False
-z = False
+def blackjack(cards):
+    card_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+                   'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
 
-if x or y and z:
-    print('Yes')
-else:
-    print('No')
+    score = 0
+    aces = 0
+
+    for card in cards:
+        score += card_values[card]
+        if card == 'A':
+            aces += 1
+
+    for i in range(aces):
+        if score > 21 and aces > 0:
+            score -= 10
+            aces -= 1
+
+    if score <= 21:
+        return score
+    else:
+        return 'Bust'
+
+
+input_cards = input("Введіть карти (без пробілів): ")
+result = blackjack(input_cards)
+
+print("Результат:", result)
