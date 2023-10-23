e45006bbfee361b48e9d3c95cc18daf2aa336883
Ihor Mykytiuk <igor14vn@gmail.com>
2023-10-23 19:47:30 +0300

    l

--- Python/lab7_7/main.py
+++ Python/lab7_7/main.py
@@ -1,6 +1,31 @@
-def to_camel_case(string):
-    words = string.split('_')
-    print(words)
+import re
 
 
-to_camel_case('print([res_square ** 2 for res_square in input_array if res_square > 18 ])')
+def to_camel_case(variable):
+    regex = re.compile(r'_[a-z]')
+    match = regex.findall(variable)
+    for m in match:
+        variable = variable.replace(m, m[1].upper())
+    return variable
+
+
+def to_snake_case(variable):
+    regex = re.compile(r'[A-Z]')
+    match = regex.findall(variable)
+    for m in match:
+        variable = variable.replace(m, '_' + m.lower())
+    return variable
+
+
+choice = input("Виберіть функцію (1 - toCamelCase, 2 - toSnakeCase): ")
+
+if choice == '1':
+    snake_case_variable = input("Введіть змінну для перетворення: ")
+    result = to_camel_case(snake_case_variable)
+    print("Результат:", result)
+elif choice == '2':
+    camel_case_variable = input("Введіть змінну для перетворення: ")
+    result = to_snake_case(camel_case_variable)
+    print("Результат:", result)
+else:
+    print("Невірний вибір")
